ede2a1d77503a4659fdd2550b66426690b98f498
mettamatt <matt@lullabot.com>
2016-12-16 06:19:58 -0500

    Clean up formatting.

--- bamboo.js
+++ bamboo.js
@@ -4,11 +4,12 @@ var request = require('request');
  * @param {object} params
  * @constructor
  */
-function BambooHR(params) {
-	this.apikey			= params.apikey;
-  this.subdomain 	= params.subdomain;
 
-  assert(params.apikey, 'apikey must be defined');
+function BambooHR(params) {
+	this.apikey = params.apikey;
+	this.subdomain = params.subdomain;
+
+	assert(params.apikey, 'apikey must be defined');
 	assert(params.subdomain, 'Subdomain must be defined');
 }
 
@@ -17,7 +18,7 @@ function BambooHR(params) {
  * @returns {vow.Promise}
  */
 BambooHR.prototype.getWhosOut = function(params, callback) {
-  return this._api('/v1/time_off/whos_out/?start='+encodeURIComponent(params.start)+'&end='+encodeURIComponent(params.end), callback);
+	return this._api('/v1/time_off/whos_out/?start=' + encodeURIComponent(params.start) + '&end=' + encodeURIComponent(params.end), callback);
 };
 
 /**
@@ -28,28 +29,27 @@ BambooHR.prototype.getWhosOut = function(params, callback) {
  * @private
  */
 BambooHR.prototype._api = function(path, callback) {
-	
+
 	var data = {
-  	url: 'https://'+ this.apikey +':x@api.bamboohr.com/api/gateway.php/' + this.subdomain + path,
+		url: 'https://' + this.apikey + ':x@api.bamboohr.com/api/gateway.php/' + this.subdomain + path,
 		headers: {
 			Accept: 'application/json'
 		}
-  };
+	};
 
- 	request.get(data, function(err, response, body) {
-   	if (!err && response.statusCode == 200) {
-			 return callback(null, JSON.parse(body));
-		}
-		else {
+	request.get(data, function(err, response, body) {
+		if (!err && response.statusCode == 200) {
+			return callback(null, JSON.parse(body));
+		} else {
 			return callback(err);
 		}
-  });
+	});
 };
 
 function assert(condition, error) {
 	if (!condition) {
-  	throw new Error('[Utili Bot Error] ' + error);
-  }
+		throw new Error('[Utili Bot Error] ' + error);
+	}
 }
 
 module.exports = BambooHR;
--- index.js
+++ index.js
@@ -1,50 +1,59 @@
 #!/usr/bin/env node
 
-var path 			= require('path');
-var Bot 			= require('slackbots');
-var datejs    = require('datejs');
-var Bamboohr	= require(path.join(__dirname, 'bamboo.js'));
-var config 		= require(path.join(__dirname, 'config.json'));
+require('datejs');
+var path = require('path');
+var Bot = require('slackbots');
+var Bamboohr = require(path.join(__dirname, 'bamboo.js'));
+var config = require(path.join(__dirname, 'config.json'));
 
-var bot 		= new Bot({token: config.token, name: 'utilibot'});
-var bamboo 	= new Bamboohr({apikey: config.bamboo_token, subdomain: config.subdomain});
- 
-var params = {icon_emoji: ':robot_face:'};
+var bot = new Bot({
+	token: config.token,
+	name: 'utilibot'
+});
+var bamboo = new Bamboohr({
+	apikey: config.bamboo_token,
+	subdomain: config.subdomain
+});
+
+var params = {
+	icon_emoji: ':robot_face:'
+};
 bot.on('start', function() {
 	bot.postMessageToChannel('general', 'Take the new OOO bot for a test drive.', params);
 });
 
 /**
- * @param {object} data 
+ * @param {object} data
  */
 bot.on('message', function(message) {
 	var text = String(message.text).toUpperCase();
-	
+
 	if (message.type === 'message') {
-		
+
 		if (text == '000?' || text == 'OOO?') {
-		  var today = Date.today().toString('yyyy-MM-dd');
-			bamboo.getWhosOut({start: today, end: today}, function(err, employees) {
+			var today = Date.today().toString('yyyy-MM-dd');
+			bamboo.getWhosOut({
+				start: today,
+				end: today
+			}, function(err, employees) {
 				var ooo = new Array();
-				ooo.push('*OOO for '+Date.today().toString('dddd, M/d/yyyy')+'*');
+				ooo.push('*OOO for ' + Date.today().toString('dddd, M/d/yyyy') + '*');
 				Object.keys(employees).forEach(function(key) {
-				  var e = employees[key];
-				  if (e.type == 'timeOff') {
-					  var ooo_date = null;
+					var e = employees[key];
+					if (e.type == 'timeOff') {
+						var ooo_date = null;
 						if (e.end != today) {
 							var date = Date.parse(e.end);
-							ooo_date = ' _(until '+ date.toString('M/d') + ')_';
+							ooo_date = ' _(until ' + date.toString('M/d') + ')_';
 						}
 
 						if (!ooo_date) {
 							ooo.push(e.name);
-						}
-						else {
+						} else {
 							ooo.push(e.name + ooo_date);
 						}
-					}
-					else if (e.type == 'holiday') {
-						ooo.push('*'+ e.name + '*');
+					} else if (e.type == 'holiday') {
+						ooo.push('*' + e.name + '*');
 					}
 				});
 				bot.postMessageToChannel('general', ooo.join('\r\n'), params);
@@ -56,40 +65,40 @@ bot.on('message', function(message) {
 			var result = text.match(regex);
 			var start = Date.parse(result[2]);
 			if (start) {
-				
+
 				if (start < Date.today()) start = start.next().year();
-				
-				bamboo.getWhosOut({start: start.toString('yyyy-MM-dd'), end: start.toString('yyyy-MM-dd')}, function(err, employees) {
+
+				bamboo.getWhosOut({
+					start: start.toString('yyyy-MM-dd'),
+					end: start.toString('yyyy-MM-dd')
+				}, function(err, employees) {
 					var ooo = new Array();
-					ooo.push('*OOO for '+start.toString('dddd, M/d/yyyy')+'*');
+					ooo.push('*OOO for ' + start.toString('dddd, M/d/yyyy') + '*');
 					Object.keys(employees).forEach(function(key) {
-					  var e = employees[key];
-					  if (e.type == 'timeOff') {
-						  var ooo_date = null;
+						var e = employees[key];
+						if (e.type == 'timeOff') {
+							var ooo_date = null;
 							if (e.end != today) {
 								var date = Date.parse(e.end);
-								ooo_date = ' _(until '+ date.toString('M/d') + ')_';
+								ooo_date = ' _(until ' + date.toString('M/d') + ')_';
 							}
 
 							if (!ooo_date) {
 								ooo.push(e.name);
-							}
-							else {
+							} else {
 								ooo.push(e.name + ooo_date);
 							}
-						}
-						else if (e.type == 'holiday') {
-							ooo.push('*'+ e.name + '*');
+						} else if (e.type == 'holiday') {
+							ooo.push('*' + e.name + '*');
 						}
 					});
 					bot.postMessageToChannel('general', ooo.join('\r\n'), params);
 				});
-				
-			}
-			else {
-				bot.postMessageToChannel('general', "Hrm. I don't understand the date: "+result[2], params);
+
+			} else {
+				bot.postMessageToChannel('general', "Hrm. I don't understand the date: " + result[2], params);
 			}
 		}
-		
+
 	}
 });
